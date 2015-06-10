2c7d121a2e060e76a3784e184ecc571baed4b97f
Mathias Schaemelhout <mathias.schaemelhout@digipolis.be>
2015-06-10 16:11:04 +0200

    Release v1.0.1

--- src/scripts/directives/awelzijn-notification-service.js
+++ src/scripts/directives/awelzijn-notification-service.js
@@ -5,7 +5,7 @@
   } catch (e) {
     module = angular.module('a-welzijn.notificationservice', ['$rootScope']);
   }
-  module.factory('NotificationService', [function ($rootScope) {
+  module.factory('awelzijn-NotificationService', [function ($rootScope) {
     function _createErrorMessages(msg, code, url) {
       var errors = [];
       if (msg && msg.error && msg.error.messages) {
